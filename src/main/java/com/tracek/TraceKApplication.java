16526392c0106a99dd519696561472c216540aa9
yujin <yujin09219@naver.com>
2026-07-29 19:17:46 +0900

    init

---
+++ src/main/java/com/tracek/TraceKApplication.java
@@ -0,0 +1,13 @@
+package com.tracek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TraceKApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(TraceKApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/tracek/TraceKApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tracek;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TraceKApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
