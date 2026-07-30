dfd41894297985b416a5436c8b5dda631be7c422
SONG YUJIN <156329445+0000yuyu@users.noreply.github.com>
2026-07-30 13:05:28 +0900

    [FEAT/SCRUM-8] 공통 엔티티 및 API 응답·예외 처리 기반 구축 (#2)
    * feat: BaseEntity 및 생성, 수정일시 관리
    
    * feat: BaseCode 및 성공, 실패 코드 추가
    
    * feat: Api 응답 및 기본 코드 추가
    
    * feat: Exception 및 에러 핸들러 추가

---
+++ src/main/java/com/tracek/global/response/code/BaseErrorCode.java
@@ -0,0 +1,5 @@
+package com.tracek.global.response.code;
+
+public interface BaseErrorCode extends BaseCode {
+
+}
---
+++ src/main/java/com/tracek/global/response/code/BaseSuccessCode.java
@@ -0,0 +1,5 @@
+package com.tracek.global.response.code;
+
+public interface BaseSuccessCode extends BaseCode {
+
+}
