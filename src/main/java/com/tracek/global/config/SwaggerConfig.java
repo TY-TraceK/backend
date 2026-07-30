b07e62f08a5900a39eddd4d3e28aff47d615f2f7
yujin <yujin09219@naver.com>
2026-07-30 18:08:06 +0900

    chore: swagger 설정

---
+++ src/main/java/com/tracek/global/config/SwaggerConfig.java
@@ -0,0 +1,32 @@
+package com.tracek.global.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+  @Bean
+  public OpenAPI openAPI() {
+    String jwtSchemeName = "jwtAuth";
+    Components components = new Components()
+        .addSecuritySchemes(jwtSchemeName, new SecurityScheme()
+            .name(jwtSchemeName)
+            .type(SecurityScheme.Type.HTTP)
+            .scheme("bearer")
+            .bearerFormat("JWT"));
+
+    Info info = new Info()
+        .title("TraceK API Document")
+        .description("TraceK API 명세서입니다.")
+        .version("v1.0.0");
+
+    return new OpenAPI()
+        .info(info)
+        .components(components);
+  }
+}
