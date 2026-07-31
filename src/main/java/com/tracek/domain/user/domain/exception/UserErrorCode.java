45407bef47d611cc914f588328f165625008dd41
yujin <yujin09219@naver.com>
2026-07-31 19:27:05 +0900

    feat : 유저 에러 코드 정의

---
+++ src/main/java/com/tracek/domain/user/domain/exception/UserErrorCode.java
@@ -0,0 +1,20 @@
+package com.tracek.domain.user.domain.exception;
+
+import com.tracek.global.response.code.BaseErrorCode;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@RequiredArgsConstructor
+public enum UserErrorCode implements BaseErrorCode {
+    USER_NOT_FOUND(HttpStatus.NOT_FOUND, "USER_001", "해당하는 유저가 없습니다."),
+    USER_NOT_AUTHORIZED(HttpStatus.UNAUTHORIZED, "USER_002", "해당 유저는 접근 권한이 없습니다."),
+    USER_NOT_ACTIVATED(HttpStatus.SERVICE_UNAVAILABLE, "USER_003", "해당 유저는 활성화 상태가 아닙니다."),
+    INVALID_EMAIL_FORMAT(HttpStatus.BAD_REQUEST, "USER_003", "올바르지 않은 이메일 형식입니다."),
+    ;
+
+    private final HttpStatus status;
+    private final String code;
+    private final String message;
+}
