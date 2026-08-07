9f67210ded05dab5826a52f4a121fdddc8712c74
yujin <yujin09219@naver.com>
2026-08-08 08:56:36 +0900

    feat : 유저 생성 메서드 추가 후 테스트 코드 작성
    [SCRUM-29]

---
+++ src/main/java/com/tracek/domain/user/application/service/impl/UserCommandServiceImpl.java
@@ -0,0 +1,37 @@
+package com.tracek.domain.user.application.service.impl;
+
+import com.tracek.domain.user.application.dto.command.SyncUserCommand;
+import com.tracek.domain.user.application.service.UserCommandService;
+import com.tracek.domain.user.domain.enums.OAuthProvider;
+import com.tracek.domain.user.domain.model.OAuthInfo;
+import com.tracek.domain.user.domain.model.User;
+import com.tracek.domain.user.domain.model.UserProfile;
+import com.tracek.domain.user.domain.repository.UserRepository;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserCommandServiceImpl implements UserCommandService {
+
+    private final UserRepository userRepository;
+
+    @Transactional
+    @Override
+    public Long registerOrUpdateUser(SyncUserCommand command) {
+        OAuthInfo oAuthInfo =
+                OAuthInfo.register(
+                        command.providerId(), OAuthProvider.valueOf(command.providerName()));
+        UserProfile userProfile =
+                UserProfile.register(command.userNickName(), command.userProfileImageUrl());
+        User user = userRepository.findByOAuthInfo(oAuthInfo).orElse(null);
+        if (user == null) {
+            user = User.createUser(oAuthInfo, command.connectedAt(), userProfile);
+            user = userRepository.save(user);
+        } else {
+            user.setUserProfile(userProfile);
+        }
+        return user.getId();
+    }
+}
