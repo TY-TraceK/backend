b41b1c12838990ffc2ae707038b7e8cbcc98b0f5
yujin <yujin09219@naver.com>
2026-08-08 16:54:24 +0900

    feat : 카카오 연결 함수 구현
    [SCRUM-22]

---
+++ src/main/java/com/tracek/domain/auth/infrastructure/dto/KakaoTokenResponse.java
@@ -0,0 +1,10 @@
+package com.tracek.domain.auth.infrastructure.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public record KakaoTokenResponse(
+        @JsonProperty("access_token") String accessToken,
+        @JsonProperty("token_type") String tokenType,
+        @JsonProperty("expires_in") Long expiresIn,
+        @JsonProperty("refresh_token") String refreshToken,
+        @JsonProperty("refresh_token_expires_in") Long refreshTokenExpiresIn) {}
