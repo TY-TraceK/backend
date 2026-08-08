9e673c18acac996878f61974ea67ef78dd987dce
yujin <yujin09219@naver.com>
2026-08-08 13:49:37 +0900

    refactor : 유저 등록 시 응답 변경 - 테스트 코드 작성 완료 [SCRUM-29]

--- src/main/java/com/tracek/domain/user/application/service/SyncUserResult.java
+++ src/main/java/com/tracek/domain/user/application/service/SyncUserResult.java
@@ -4,13 +4,14 @@ import com.tracek.domain.user.domain.model.User;
 import lombok.Builder;
 
 @Builder
-public record SyncUserResult(long userId, String userName, String userRole) {
+public record SyncUserResult(long userId, boolean isNewUser, String userName, String userRole) {
 
-    public static SyncUserResult from(User user) {
-        return SyncUserResult.builder()
-                .userId(user.getId())
-                .userName(user.getUserProfile().getNickname())
-                .userName(user.getUserRole().name())
-                .build();
-    }
+  public static SyncUserResult from(User user, boolean isNewUser) {
+    return SyncUserResult.builder()
+        .userId(user.getId())
+        .isNewUser(isNewUser)
+        .userName(user.getUserProfile().getNickname())
+        .userName(user.getUserRole().name())
+        .build();
+  }
 }
