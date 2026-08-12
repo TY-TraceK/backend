4e0ef5ee8e19b2638db1eaee2a45eb67a4707115
yujin <yujin09219@naver.com>
2026-08-12 20:05:02 +0900

    refactor : voteErrorCode 상태 코드 변경
    [SCRUM-35]

--- src/main/java/com/tracek/domain/vote/domain/exception/VoteErrorCode.java
+++ src/main/java/com/tracek/domain/vote/domain/exception/VoteErrorCode.java
@@ -12,7 +12,7 @@ public enum VoteErrorCode implements BaseErrorCode {
     VOTE_TARGET_NOT_FOUND(
             HttpStatus.NOT_FOUND, "VOTE_002", "존재하지 않거나 유효하지 않은 투표 대상(관광지/콘텐츠/아티스트 조합)입니다."),
 
-    DUPLICATE_VOTE(HttpStatus.CONFLICT, "VOTE_003", "해당 관광지에 이미 진행한 유효한 투표가 존재합니다."),
+    ALREADY_VOTED(HttpStatus.BAD_REQUEST, "VOTE_003", "해당 관광지에 이미 진행한 유효한 투표가 존재합니다."),
     VOTE_ALREADY_CANCELLED(HttpStatus.BAD_REQUEST, "VOTE_004", "이미 취소 처리된 투표입니다."),
     VOTE_CANNOT_BE_CANCELLED(HttpStatus.BAD_REQUEST, "VOTE_005", "취소할 수 없거나 취소 가능 기간이 지난 투표입니다."),
     UNAUTHORIZED_VOTE_ACCESS(HttpStatus.FORBIDDEN, "VOTE_006", "본인의 투표 이력만 취소하거나 수정할 수 있습니다."),
