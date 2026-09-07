93b774c34b51c2c2d5047a3d3bc86b858674ed5c
yujin <yujin09219@naver.com>
2026-09-07 18:58:12 +0900

    feat : 랭킹 도메인 레포 함수 추가

---
+++ src/main/java/com/tracek/domain/ranking/infrastructure/persistence/ArtistLocationVisitRankingJpaRepository.java
@@ -0,0 +1,11 @@
+package com.tracek.domain.ranking.infrastructure.persistence;
+
+import com.tracek.domain.ranking.domain.model.ArtistLocationVisitRanking;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ArtistLocationVisitRankingJpaRepository extends
+    JpaRepository<ArtistLocationVisitRanking, Long> {
+
+  Optional<ArtistLocationVisitRanking> findByLocationIdAndArtistId(Long locationId, Long artistId);
+}
---
+++ src/main/java/com/tracek/domain/ranking/infrastructure/persistence/ContentLocationVisitRankingRepositoryImpl.java
@@ -0,0 +1,27 @@
+package com.tracek.domain.ranking.infrastructure.persistence;
+
+import com.tracek.domain.ranking.domain.model.ContentLocationVisitRanking;
+import com.tracek.domain.ranking.domain.repository.ContentLocationVisitRankingRepository;
+import java.util.Optional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class ContentLocationVisitRankingRepositoryImpl implements
+    ContentLocationVisitRankingRepository {
+
+  private final ContentLocationVisitRankingJpaRepository contentLocationVisitRankingJpaRepository;
+
+  @Override
+  public Optional<ContentLocationVisitRanking> findByLocationIdAndContentId(Long locationId,
+      Long contentId) {
+    return contentLocationVisitRankingJpaRepository.findByLocationIdAndContentId(locationId,
+        contentId);
+  }
+
+  @Override
+  public ContentLocationVisitRanking save(ContentLocationVisitRanking contentLocationVisitRanking) {
+    return contentLocationVisitRankingJpaRepository.save(contentLocationVisitRanking);
+  }
+}
