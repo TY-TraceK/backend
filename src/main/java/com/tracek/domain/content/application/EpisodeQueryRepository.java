6f0cadb77aa577550345b4cd99eec76aded3c139
Yujin <121761705+YujinB@users.noreply.github.com>
2026-09-09 00:21:29 +0900

    SCRUM-72 feat: 관광지-콘텐츠(-아티스트) 연관 여부 확인 기능 추가 (#135)

--- src/main/java/com/tracek/domain/content/application/EpisodeQueryRepository.java
+++ src/main/java/com/tracek/domain/content/application/EpisodeQueryRepository.java
@@ -45,4 +45,30 @@ public class EpisodeQueryRepository {
                                         t.get(episode.content.id), t.get(episodeArtist.artist.id)))
                 .toList();
     }
+
+    public Boolean isRelatedContent(Long locationId, Long contentId) {
+        return queryFactory
+                        .selectOne()
+                        .from(episodeLocation)
+                        .where(
+                                episodeLocation.location.id.eq(locationId),
+                                episodeLocation.episode.content.id.eq(contentId))
+                        .fetchFirst()
+                != null;
+    }
+
+    public Boolean isRelatedContentAndArtist(Long locationId, Long contentId, Long artistId) {
+        return queryFactory
+                        .selectOne()
+                        .from(episodeLocation)
+                        .join(episodeLocation.episode, episode)
+                        .join(episodeArtist)
+                        .on(episode.id.eq(episodeArtist.episode.id))
+                        .where(
+                                episodeLocation.location.id.eq(locationId),
+                                episode.content.id.eq(contentId),
+                                episodeArtist.artist.id.eq(artistId))
+                        .fetchFirst()
+                != null;
+    }
 }
