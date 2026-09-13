26b823a7fb2ff6264b88336c9407928f48ab52df
yujin <yujin09219@naver.com>
2026-09-13 16:27:01 +0900

    refactor: 테스트 컨트롤러 및 테스트 코드 수정
    [SCRUM-80]

--- src/main/java/com/tracek/global/common/TestController.java
+++ src/main/java/com/tracek/global/common/TestController.java
@@ -1,12 +1,25 @@
 package com.tracek.global.common;
 
 import com.tracek.domain.auth.application.service.OAuthService;
+import com.tracek.domain.ranking.application.dto.condition.RankingCondition;
+import com.tracek.domain.ranking.application.dto.result.RankingSliceResult;
+import com.tracek.domain.ranking.application.dto.result.RelatedArtistRankingResult;
+import com.tracek.domain.ranking.application.dto.result.RelatedContentRankingResult;
+import com.tracek.domain.ranking.application.dto.result.RelatedLocationRankingResult;
+import com.tracek.domain.ranking.application.dto.result.RelatedMultiRankingResult;
+import com.tracek.domain.ranking.application.service.VisitRankingProjectionService;
+import com.tracek.domain.ranking.application.service.VisitRankingRelatedQueryService;
 import com.tracek.global.response.ApiResponse;
 import com.tracek.global.response.GeneralSuccessCode;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Profile;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -16,11 +29,131 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController implements TestControllerDocs {
 
     private final OAuthService oAuthService;
+    private final VisitRankingRelatedQueryService visitRankingRelatedQueryService;
+    private final VisitRankingProjectionService visitRankingProjectionService;
 
     @PostMapping("/auth/token/{userId}")
-    public ApiResponse<String> generateDevToken(Long userId) {
-
+    public ApiResponse<String> generateDevToken(@PathVariable Long userId) {
         return ApiResponse.success(
                 GeneralSuccessCode.OK, oAuthService.getUserAndAccessToken(userId));
+    }
+
+    @GetMapping("/auth/success")
+    @PreAuthorize("isAuthenticated()")
+    @SecurityRequirement(name = "jwtAuth")
+    public String getTestAuthWithSuccess() {
+        return "인증 성공";
+    }
+
+    @PostMapping("/ranking/projection/increase")
+    public ApiResponse<Void> increaseRankingProjection(
+            @RequestParam Long locationId,
+            @RequestParam(required = false) Long contentId,
+            @RequestParam(required = false) Long artistId) {
+        visitRankingProjectionService.increase(locationId, contentId, artistId);
+
+        return ApiResponse.success(GeneralSuccessCode.OK, null);
+    }
+
+    @PostMapping("/ranking/projection/decrease")
+    public ApiResponse<Void> decreaseRankingProjection(
+            @RequestParam Long locationId,
+            @RequestParam(required = false) Long contentId,
+            @RequestParam(required = false) Long artistId) {
+        visitRankingProjectionService.decrease(locationId, contentId, artistId);
+
+        return ApiResponse.success(GeneralSuccessCode.OK, null);
+    }
+
+    @GetMapping("/ranking/content/{contentId}/artists")
+    public RankingSliceResult<RelatedArtistRankingResult> getArtistsByContent(
+            @PathVariable Long contentId,
+            @RequestParam(required = false) Long lastCount,
+            @RequestParam(required = false) Long lastId,
+            @RequestParam(defaultValue = "10") Integer size) {
+        RankingCondition condition = new RankingCondition(lastCount, lastId, size);
+
+        return visitRankingRelatedQueryService.getArtistsByContent(contentId, condition);
+    }
+
+    @GetMapping("/ranking/content/{contentId}/locations")
+    public RankingSliceResult<RelatedLocationRankingResult> getLocationsByContent(
+            @PathVariable Long contentId,
+            @RequestParam(required = false) Long lastCount,
+            @RequestParam(required = false) Long lastId,
+            @RequestParam(defaultValue = "10") Integer size) {
+        RankingCondition condition = new RankingCondition(lastCount, lastId, size);
+
+        return visitRankingRelatedQueryService.getLocationsByContent(contentId, condition);
+    }
+
+    @GetMapping("/ranking/content/{contentId}/multi")
+    public RankingSliceResult<RelatedMultiRankingResult> getMultiRankingByContent(
+            @PathVariable Long contentId) {
+        return visitRankingRelatedQueryService.getMultiRankingByContent(contentId);
+    }
+
+    /*
+     * Artist 기준
+     */
+
+    @GetMapping("/ranking/artist/{artistId}/locations")
+    public RankingSliceResult<RelatedLocationRankingResult> getLocationsByArtist(
+            @PathVariable Long artistId,
+            @RequestParam(required = false) Long lastCount,
+            @RequestParam(required = false) Long lastId,
+            @RequestParam(defaultValue = "10") Integer size) {
+        RankingCondition condition = new RankingCondition(lastCount, lastId, size);
+
+        return visitRankingRelatedQueryService.getLocationsByArtist(artistId, condition);
+    }
+
+    @GetMapping("/ranking/artist/{artistId}/contents")
+    public RankingSliceResult<RelatedContentRankingResult> getContentsByArtist(
+            @PathVariable Long artistId,
+            @RequestParam(required = false) Long lastCount,
+            @RequestParam(required = false) Long lastId,
+            @RequestParam(defaultValue = "10") Integer size) {
+        RankingCondition condition = new RankingCondition(lastCount, lastId, size);
+
+        return visitRankingRelatedQueryService.getContentsByArtist(artistId, condition);
+    }
+
+    @GetMapping("/ranking/artist/{artistId}/multi")
+    public RankingSliceResult<RelatedMultiRankingResult> getMultiRankingByArtist(
+            @PathVariable Long artistId) {
+        return visitRankingRelatedQueryService.getMultiRankingByArtist(artistId);
+    }
+
+    /*
+     * Location 기준
+     */
+
+    @GetMapping("/ranking/location/{locationId}/artists")
+    public RankingSliceResult<RelatedArtistRankingResult> getArtistsByLocation(
+            @PathVariable Long locationId,
+            @RequestParam(required = false) Long lastCount,
+            @RequestParam(required = false) Long lastId,
+            @RequestParam(defaultValue = "10") Integer size) {
+        RankingCondition condition = new RankingCondition(lastCount, lastId, size);
+
+        return visitRankingRelatedQueryService.getArtistsByLocation(locationId, condition);
+    }
+
+    @GetMapping("/ranking/location/{locationId}/contents")
+    public RankingSliceResult<RelatedContentRankingResult> getContentsByLocation(
+            @PathVariable Long locationId,
+            @RequestParam(required = false) Long lastCount,
+            @RequestParam(required = false) Long lastId,
+            @RequestParam(defaultValue = "10") Integer size) {
+        RankingCondition condition = new RankingCondition(lastCount, lastId, size);
+
+        return visitRankingRelatedQueryService.getContentsByLocation(locationId, condition);
+    }
+
+    @GetMapping("/ranking/location/{locationId}/multi")
+    public RankingSliceResult<RelatedMultiRankingResult> getMultiRankingByLocation(
+            @PathVariable Long locationId) {
+        return visitRankingRelatedQueryService.getMultiRankingByLocation(locationId);
     }
 }
