d925733081ef01330878f689a5f8167f01ba3137
yujin <yujin09219@naver.com>
2026-09-15 17:42:54 +0900

    feat : 방문 인증 업데이트 서비스 로직 추가 및 방문 인증 제약조건 수정
    [SCRUM-77]

--- src/main/java/com/tracek/domain/visitVerification/application/service/VisitVerificationCommandService.java
+++ src/main/java/com/tracek/domain/visitVerification/application/service/VisitVerificationCommandService.java
@@ -2,12 +2,18 @@ package com.tracek.domain.visitVerification.application.service;
 
 import com.tracek.domain.visitVerification.application.dto.command.VisitVerificationCancelCommand;
 import com.tracek.domain.visitVerification.application.dto.command.VisitVerificationCreateCommand;
+import com.tracek.domain.visitVerification.application.dto.command.VisitVerificationUpdateCommand;
 import com.tracek.domain.visitVerification.application.dto.result.VisitVerificationCancelResult;
 import com.tracek.domain.visitVerification.application.dto.result.VisitVerificationCreateResult;
+import com.tracek.domain.visitVerification.application.dto.result.VisitVerificationUpdateResult;
+import jakarta.transaction.Transactional;
 
 public interface VisitVerificationCommandService {
 
     VisitVerificationCreateResult createVisitVerification(VisitVerificationCreateCommand command);
 
     VisitVerificationCancelResult cancelVisitVerification(VisitVerificationCancelCommand command);
+
+    @Transactional
+    VisitVerificationUpdateResult updateVisitVerification(VisitVerificationUpdateCommand command);
 }
