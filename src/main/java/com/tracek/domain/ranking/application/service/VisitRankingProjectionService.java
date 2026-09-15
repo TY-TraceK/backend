d6b0308b6ff15511cd692054fa6c63e034687c66
yujin <yujin09219@naver.com>
2026-09-15 17:46:30 +0900

    feat : 방문 인증 수정 이벤트 추가에 따른 랭킹 리스너 함수 추가 및 처리
    [SCRUM-77]

--- src/main/java/com/tracek/domain/ranking/application/service/VisitRankingProjectionService.java
+++ src/main/java/com/tracek/domain/ranking/application/service/VisitRankingProjectionService.java
@@ -1,8 +1,19 @@
 package com.tracek.domain.ranking.application.service;
 
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
 public interface VisitRankingProjectionService {
 
     void increase(Long locationId, Long contentId, Long artistId);
 
     void decrease(Long locationId, Long contentId, Long artistId);
+
+    @Transactional(propagation = Propagation.REQUIRES_NEW)
+    void update(
+            Long locationId,
+            Long previousContentId,
+            Long previousArtistId,
+            Long updatedContentId,
+            Long updatedArtistId);
 }
