89747d3d8208f9c9a3bcf3b4482d0805e62ebc0b
yujin <yujin09219@naver.com>
2026-09-15 10:56:54 +0900

    feat : 레포 함수 추가
    [SCRUM-80]

--- src/main/java/com/tracek/domain/ranking/domain/repository/LocationVisitRankingRepository.java
+++ src/main/java/com/tracek/domain/ranking/domain/repository/LocationVisitRankingRepository.java
@@ -1,6 +1,9 @@
 package com.tracek.domain.ranking.domain.repository;
 
+import com.tracek.domain.ranking.domain.model.LocationRankingView;
 import com.tracek.domain.ranking.domain.model.LocationVisitRanking;
+import com.tracek.domain.ranking.domain.model.RankingSearchCriteria;
+import java.util.List;
 import java.util.Optional;
 import org.springframework.stereotype.Repository;
 
@@ -14,4 +17,6 @@ public interface LocationVisitRankingRepository {
     void increaseVerificationCount(Long locationId);
 
     void decreaseVerificationCount(Long locationId);
+
+    List<LocationRankingView> findLocationRankingsByRegion(RankingSearchCriteria<String> criteria);
 }
