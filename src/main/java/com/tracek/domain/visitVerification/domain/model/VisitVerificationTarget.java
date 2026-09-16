62fbb0fbba35da6547109926f4e1ba354150e060
yujin <yujin09219@naver.com>
2026-09-16 18:16:23 +0900

    refactor : 방문 인증 제약조건 변경에 따른 모델 수정
    [SCRUM-79]

--- src/main/java/com/tracek/domain/visitVerification/domain/model/VisitVerificationTarget.java
+++ src/main/java/com/tracek/domain/visitVerification/domain/model/VisitVerificationTarget.java
@@ -1,7 +1,14 @@
 package com.tracek.domain.visitVerification.domain.model;
 
+import jakarta.persistence.CollectionTable;
 import jakarta.persistence.Column;
+import jakarta.persistence.ElementCollection;
 import jakarta.persistence.Embeddable;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.UniqueConstraint;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -13,12 +20,20 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class VisitVerificationTarget {
 
-    @Column() private Long artistId;
+    @ElementCollection
+    @CollectionTable(
+            name = "visit_verification_target_artist",
+            joinColumns = @JoinColumn(name = "visit_verification_id"),
+            uniqueConstraints = {
+                @UniqueConstraint(columnNames = {"visit_verification_id", "artist_id"})
+            })
+    @Column(name = "artist_id")
+    private Set<Long> artistIds = new HashSet<>();
 
     @Column(nullable = false)
     private Long contentId;
 
-    public static VisitVerificationTarget of(Long artistId, Long contentId) {
-        return new VisitVerificationTarget(artistId, contentId);
+    public static VisitVerificationTarget of(Collection<Long> artistIds, Long contentId) {
+        return new VisitVerificationTarget(new HashSet<>(artistIds), contentId);
     }
 }
