15d1fe09d0983fe3831ad60040018d4003f488a7
yujin <yujin09219@naver.com>
2026-09-17 07:34:08 +0900

    refactor : 모델 클래스 -> 레코드 변경
    [SCRUM-97]

--- src/main/java/com/tracek/domain/fan/domain/model/ArtistFanId.java
+++ src/main/java/com/tracek/domain/fan/domain/model/ArtistFanId.java
@@ -1,19 +1,3 @@
 package com.tracek.domain.fan.domain.model;
 
-import jakarta.persistence.Embeddable;
-import java.io.Serializable;
-import lombok.AllArgsConstructor;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-
-@NoArgsConstructor
-@AllArgsConstructor
-@EqualsAndHashCode
-@Embeddable
-@Getter
-public class ArtistFanId implements Serializable {
-
-    private Long userId;
-    private Long artistId;
-}
+public record ArtistFanId(Long userId, Long artistId) {}
