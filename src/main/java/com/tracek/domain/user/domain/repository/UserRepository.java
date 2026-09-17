9bf980f234e9c26f31c33d08f6e553d89ed671fd
yujin <yujin09219@naver.com>
2026-09-18 03:08:13 +0900

    feat: 도메인 모델 수정
    [SCRUM-105]

--- src/main/java/com/tracek/domain/user/domain/repository/UserRepository.java
+++ src/main/java/com/tracek/domain/user/domain/repository/UserRepository.java
@@ -3,6 +3,7 @@ package com.tracek.domain.user.domain.repository;
 import com.tracek.domain.user.domain.enums.UserStatus;
 import com.tracek.domain.user.domain.model.OAuthInfo;
 import com.tracek.domain.user.domain.model.User;
+import com.tracek.domain.user.domain.model.UserActivityProjection;
 import java.util.Optional;
 
 public interface UserRepository {
@@ -14,4 +15,6 @@ public interface UserRepository {
     Optional<User> findById(Long userId);
 
     User save(User user);
+
+    UserActivityProjection findUserActivity(Long userId);
 }
