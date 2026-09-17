ffff5a18a53736bf8d35bd64886a50a34f7c3e17
yujin <yujin09219@naver.com>
2026-09-17 16:05:58 +0900

    feat : 테스트 환경 에러 수정
    [SCRUM-100]

--- src/main/java/com/tracek/domain/user/infrastructure/storage/LocalStorageClient.java
+++ src/main/java/com/tracek/domain/user/infrastructure/storage/LocalStorageClient.java
@@ -12,7 +12,7 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
 
 @Component
-@Profile("local")
+@Profile({"local", "test"})
 public class LocalStorageClient implements StorageClient {
 
     private final Path rootPath;
